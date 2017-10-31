a7a2851a96050a7bbbe89dcc902f87095cf0ec5e
wqer1019 <2013855675@qq.com>
2017-10-31 15:23:39 +0800

    '更新到原作者的主分支'

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,13 +16,13 @@ class DatabaseSeeder extends Seeder
             factory(\App\Models\Category::class, random_int(0, 3))->create(['parent_id' => $category->id]);
         });
 
-        factory(App\Models\User::class, 10)->create()->each(function ($user) {
+        factory(App\Models\User::class, 3)->create()->each(function ($user) {
             factory(App\Models\Post::class, random_int(0, 10))->create(['user_id' => $user->id])->each(function ($post) {
                 $post->postContent()->save(factory(PostContent::class)->make());
             });
         });
 
-        DB::table('users')->where('id', 1)->update(['user_name' => 'tiny', 'email' => 'tiny@test.com', 'locked_at' => null]);
+        DB::table('users')->where('id', 1)->update(['user_name' => 'tiny', 'password'=>bcrypt('tiny'), 'email' => 'tiny@test.com', 'locked_at' => null]);
 
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
